5029eb10621bf506e8a7c8d46a942a27bb60989a
DeepBlueBW <deepbluebw@gmx.net>
2021-03-21 12:09:25 +0100

    function to add quotes

--- get-quote.py
+++ get-quote.py
@@ -12,5 +12,15 @@ def primary():
       rnd = random.randint(0, last)
       print(quotes[rnd], end="")
 
+def add():
+  # adding of a new quote to the quote.txt
+
+  quote = input("Enter a new quote: " )
+  f = open("quotes.txt", "a")
+  f.write(quote)
+  f.close()
+  print("The quote: \n" + quote+ "\nis added to the File quote.txt.")
+
 if __name__== "__main__":
   primary()
+  add()
